fe5e53c611de00c87bd9dd23a99d6986aa5827e7
janainaemilia <=>
2018-08-25 20:51:54 -0300

    Fist Commit

---
+++ Aula 01/HelloWorldCCP3ANBUA/app/src/main/java/br/com/dev/helloworldccp3anbua/MainActivity.java
@@ -0,0 +1,15 @@
+package br.com.dev.helloworldccp3anbua;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity { //AppCompatActivity relacionado a retrocompatibilidade, para utilizar componentes destinado à tablet em versões anteriores a 3 do android studio
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // metodo  responsavel por "inflar a view", gerar a arvore a ser construida para o render da view
+        setContentView(R.layout.activity_main); // R: classe publica gerada por padrão, que contem classes aninhadas (no caso, layout) e gera uma constante
+    }
+}
